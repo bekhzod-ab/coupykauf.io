938565eba14170020b5a46e897ffd1fd4a415bb9
Sandra Almoli <almolisandra@gmail.com>
2021-04-21 14:01:33 +0200

    static login

--- front-end/src/pages/SignUpPage.js
+++ front-end/src/pages/SignUpPage.js
@@ -1,7 +1,9 @@
 const SignUpPage = () => {
+
     return (
         <div className="signup-page"> 
             <h2> Welcome to the Sign Up Page </h2>
+            <div className="signup-container"> </div> 
         </div> 
     )
 }
