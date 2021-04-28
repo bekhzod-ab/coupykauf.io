e21215e867641fdd437e4e39d19f9cb6dbcdba7c
karllanger <karl431@web.de>
2021-04-28 15:35:02 +0200

    modifiedy navbar and created homepage

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,26 +3,30 @@ import Footer from "./components/Footer"
 import HomePage from "./pages/HomePage";
 import AboutUsPage from "./pages/AboutUsPage";
 import VouchersPage from "./pages/VouchersPage";
-import Purchase from "./pages/PurchaseVoucher"
+/* import Purchase from "./pages/PurchaseVoucher" */
 import FaqPage from "./pages/FaqPage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import ContactPage from "./pages/ContactPage";
 import './index.css';
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { useState } from "react"
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(true);
+  
+
   return (
     
     <Router>
        <div className="app">
-         <Navbar/> 
+         <Navbar loggedIn = { loggedIn } setLoggedIn = { setLoggedIn }/> {/* exporting the variables to the navbarcomponent */}
          <div className="wrapper"> {/* this is for fixing the footer to the bottom */}
           <Switch> 
               <Route exact path="/"> <HomePage/> </Route>
               <Route exact path="/about"> <AboutUsPage/> </Route>
               <Route exact path="/vouchers"> <VouchersPage/> </Route>
-              <Route exact path="/vouchers/:articleNo"><Purchase/></Route>
+            {/*   <Route exact path="/vouchers/:articleNo"><Purchase/></Route> */}
               <Route exact path="/faq"> <FaqPage/> </Route>
               <Route exact path="/contact"> <ContactPage/> </Route>
               <Route exact path="/signup"> <SignUpPage/> </Route>
--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -1,16 +1,28 @@
 import {Link} from "react-router-dom"
 
-const Navbar = () => {
+const Navbar = ({ loggedIn, setLoggedIn }) => {
+    function logOut(){
+        setLoggedIn(false)
+    }
     return (
         <nav className="navbar">
         <h1>CoupyKauf</h1>
-        <div className="links">
+        {/* <div className="links"> */}
+            <div className="navnav">
             <Link to="/" className="link"> Home </Link>
             <Link to="/about" className="link"> About us</Link>
             <Link to="/vouchers" className="link"> Find Voucher</Link>
-            <Link to="/signup" className="link"> Sign up </Link>
-            <Link to="/login" className="link"> Log in </Link>
-        </div> 
+            </div>
+            <div>
+            {loggedIn ?
+            <><span>HELLO USER! </span>
+            <button onClick={logOut}>log out</button> </> :
+            <>{/* we use the react fragment to display two elements like one (cant use a ternary on two elements)*/}
+            <Link to="/signup" className="link"> Sign up </Link> 
+            <Link to="/login" className="link"> Log in </Link> </>}
+            </div>
+            
+        {/* </div>  */}
         </nav>
     )
 }
