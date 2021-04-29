e6edc1a5dcac4f441cdbae879295260e26e453a2
bekhzod-ab <bekhzod.ab@gmail.com>
2021-04-29 13:39:49 +0200

    fs commands added

--- back-end/server.js
+++ back-end/server.js
@@ -3,6 +3,9 @@
 
 require("dotenv").config();
 const cors = require("cors")
+const fs = require("fs")
+const dir = "./images"
+const path = require("path")
 const mongoose = require("mongoose")
 const cookieParser = require("cookie-parser")
 const express = require("express")
@@ -41,6 +44,12 @@ app.use(express.urlencoded({extended: true}))
 //Home page
 app.get("/", (req,res) => {
     res.status(200).send("Main Page")
+})
+app.use("/images", express.static(path.join(__dirname, "images")))
+
+app.post("/images", async(req,res) => {
+    fs.mkdir(dir, res.locals.email)
+    fs.appendFile(path.join(__dirname, `images/${dir}`, req.body.image))
 })
 
 
