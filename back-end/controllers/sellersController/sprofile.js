b738ed9b31cfa409104cb131b3453fd20f30e7c8
bekhzod-ab <bekhzod.ab@gmail.com>
2021-04-29 10:05:32 +0200

    deleted send file

--- back-end/controllers/sellersController/sprofile.js
+++ back-end/controllers/sellersController/sprofile.js
@@ -1,10 +1,9 @@
-const path = require("path")
+
 const {profile} = require("../../models/sellersModel.js")
 
 
 
 const DisplayProfile = async(req,res) => {
-    res.sendfile() 
     res.status(200).json(await profile(res.locals.email))
 }
 
