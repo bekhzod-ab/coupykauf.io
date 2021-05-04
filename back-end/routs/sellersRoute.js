dc169a51ba249a2ae15b24dac10a5facc16d07ae
bekhzod-ab <bekhzod.ab@gmail.com>
2021-05-04 15:11:46 +0200

    img upload in created dir

--- back-end/routs/sellersRoute.js
+++ back-end/routs/sellersRoute.js
@@ -1,4 +1,6 @@
 const router = require(`express`).Router()
+const fs = require("fs")
+const dir = "./images"
 const signIn = require("../controllers/sellersController/slogin.js")
 const sRegister = require("../controllers/sellersController/sregistration.js")
 const DisplayProfile = require("../controllers/sellersController/sprofile.js")
@@ -13,5 +15,27 @@ module.exports = () => {
     router.post("/signup", sRegister)
     router.get("/profile",validator,DisplayProfile)
     router.post("/profile",validator,UpdateProfile)
+
+    //  1) If there is no folder -> Create subfolder naming accordingly
+    //  2) append req.files.imageUrl init in any case
+    // /images/{ident from res.locals}
+
+
+        router.post("/image", validator, async(req,res) => {
+            console.log(req.files)
+            if(!fs.existsSync(`images/${res.locals.email}`)) {
+                fs.mkdirSync(`images/${res.locals.email}`)
+                console.log("there is ")}
+            fs.writeFileSync(`images/${res.locals.email}/${req.files.gallery_Url.name}`, req.files.gallery_Url.data, {mode: 0o755})    
+                
+            // {fs.mkdir(dir, res.locals.email)}
+            // fs.appendFile(path.join(__dirname, `images/${dir}`, req.body.image))
+        })
+
+
+
+
+
+
     return router
 }
