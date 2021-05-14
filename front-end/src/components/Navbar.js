6713cfbb7b8c8a05d4eebff4c9bf57033f7f10f9
karllanger <karl431@web.de>
2021-05-14 14:58:39 +0200

    added mobilenav

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -1,31 +1,37 @@
 import {Link} from "react-router-dom"
+import MobileNav from "./MobileNavbar"
+
 
 const Navbar = ({ loggedIn, setLoggedIn }) => {
     function logOut(){
         setLoggedIn(false)
     }
     return (
-        <nav className="navbar">
-            {/*            maybe this shouldnt be h1              */}
-            <h2>CoupyKauf</h2> 
-        {/* <div className="links"> */}
-            <div className="navnav">
-            <Link to="/" className="link"> Home </Link>
-            <Link to="/about" className="link"> About us</Link>
-            <Link to="/vouchers" className="link"> Find Voucher</Link>
-            </div>
-            <div>
-            {loggedIn ?
-            <><span> <Link to="/profile" className="link"> My profile </Link></span>
-            <button onClick={logOut}>log out</button> </> :
-            <>{/* we use the react fragment to display two elements like one (cant use a ternary on two elements)*/}
-            <Link to="/signup" className="link"> Sign up </Link> 
-            <Link to="/login" className="link"> Log in </Link> </>}
-            </div>
             
+        <nav className="navbar">
+            <MobileNav/>
+            <div className="bigScreenNav">
+                <h2>CoupyKauf</h2> 
+            {/* <div className="links"> */}
+                <div className="navnav">
+                <Link to="/" className="link"> Home </Link>
+                <Link to="/about" className="link"> About us</Link>
+                <Link to="/vouchers" className="link"> Find Voucher</Link>
+                </div>
+                <div>
+                {loggedIn ?
+                <><span> <Link to="/profile" className="link"> My profile </Link></span>
+                <button onClick={logOut}>log out</button> </> :
+                <>{/* we use the react fragment to display two elements like one (cant use a ternary on two elements)*/}
+                <Link to="/signup" className="link"> Sign up </Link> 
+                <Link to="/login" className="link"> Log in </Link> </>}
+                </div>
+            </div>
         {/* </div>  */}
+        
         </nav>
     )
 }
+
 
 export default Navbar
