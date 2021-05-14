561309fbcd93c1ba976926a312912412d69c8c1f
karllanger <karl431@web.de>
2021-05-14 15:28:30 +0200

    navbar merge conflict

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -19,31 +19,15 @@ const Navbar = () => {
 
 
     return (
-            
         <nav className="navbar">
             <MobileNav/>
-            <div className="bigScreenNav">
-                <h2>CoupyKauf</h2> 
-            {/* <div className="links"> */}
-                <div className="navnav">
-                <Link to="/" className="link"> Home </Link>
-                <Link to="/about" className="link"> About us</Link>
-                <Link to="/vouchers" className="link"> Find Voucher</Link>
-                </div>
-                <div>
-                {loggedIn ?
-                <><span> <Link to="/profile" className="link"> My profile </Link></span>
-                <button onClick={logOut}>log out</button> </> :
-                <>{/* we use the react fragment to display two elements like one (cant use a ternary on two elements)*/}
-                <Link to="/signup" className="link"> Sign up </Link> 
-                <Link to="/login" className="link"> Log in </Link> </>}
-                </div>
+            {/* s */}
             <Link to="/" className="link logo-container"> 
             <img src={logo} className="logo" alt="logo"/> 
             <h2>CoupyKauf</h2>
             </Link>
           
-            {/* <div className="links"> */}
+        {/* <div className="links"> */}
             <div className="navnav">
             <Link to="/" className="link"> Home </Link>
             <Link to="/about" className="link"> About us</Link>
@@ -57,8 +41,8 @@ const Navbar = () => {
             <Link to="/signup" className="link"> Sign up </Link> 
             <Link to="/login" className="link"> Log in </Link> </>}
             </div>
-
-        </div> 
+            {/* </div> */}
+        {/* </div>  */}
         
         </nav>
     )
