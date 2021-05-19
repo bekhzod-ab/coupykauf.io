067092399c366e0d5d59424f2aa5d1fae6bb0026
bekhzod-ab <bekhzod.ab@gmail.com>
2021-05-19 12:04:22 +0200

    cards changed

--- front-end/src/components/Card.js
+++ front-end/src/components/Card.js
@@ -1,16 +1,16 @@
+import {Link} from "react-router-dom"
+
 const Card = ({voucher}) => {
     return (  
         <div className="card">
 
         <div className="profile-sidebar">
-          <img src={voucher.gallery_Url1} alt=""/>
-          <div> isjfsfjslfjlsd</div>
-          
+          <img src={voucher.images_array[1]} alt=""/>
         </div>
         <div className="profile-main">
           <h2>{ voucher.company_name}</h2>
           <p className="message">{voucher.description}</p>
-          
+          <Link to={`/vouchers/${voucher.company_name}`}>More</Link><li/>
         </div>
         
 
