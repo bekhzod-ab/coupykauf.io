911859da69fbcc07ea1f7a58d05964a7e2993e59
karllanger <karl431@web.de>
2021-05-27 14:25:05 +0200

    i dont know what

--- front-end/src/components/Card.js
+++ front-end/src/components/Card.js
@@ -1,4 +1,5 @@
 import {Link} from "react-router-dom"
+import { FaFacebook, FaInstagram } from "react-icons/fa"
 
 const Card = ({voucher}) => {
     return (  
@@ -6,11 +7,17 @@ const Card = ({voucher}) => {
 
         <div className="profile-sidebar">
           <img src={voucher.images_array[0]} alt=""/>
+          <p> {voucher.category} </p>
+          <p> {voucher.phone} </p>
+          <p> {voucher.address} </p>
+          <a ><FaFacebook className="icon" /></a> 
+                <a > <FaInstagram className="icon"/> </a> 
         </div>
         <div className="profile-main">
           <h2>{ voucher.company_name}</h2>
           <p className="message">{voucher.description}</p>
-          <Link to={`/vouchers/${voucher.company_name}`}>More</Link><li/>
+        <Link className="btnCard" to={`/vouchers/${voucher.company_name}`}>BUY</Link><li/>
+          
         </div>
         
 
