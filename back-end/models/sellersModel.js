3f4f098a9bccdc6147e609d262497d57d59ef4f8
bekhzod-ab <bekhzod.ab@gmail.com>
2021-05-27 14:25:19 +0200

    bic fixed

--- back-end/models/sellersModel.js
+++ back-end/models/sellersModel.js
@@ -27,6 +27,10 @@ const sellerSchema = new mongoose.Schema({
         type: String,
         default: null
     },
+    BIC : {
+        type: String,
+        default: null
+    },
     amountof10: {
         type: Number,
         default: null
