ab8edfb22d482dcc039417b604d5ba2df4d73de5
karllanger <karl431@web.de>
2021-06-02 13:30:30 +0200

    purchasepage, added some styling

--- front-end/src/pages/PurchaseVoucher.js
+++ front-end/src/pages/PurchaseVoucher.js
@@ -12,15 +12,15 @@ export default function Purchase() {
     console.log(details)
     return (
         <div className="voucher_details">
-            <div className="voucher">
-                <ul>
-                    <li>voucher: {details.company_name} </li>
-                    <li>description: {details.description} </li>
-                    <li>available 10 € Vouchers: {details.amountof10}</li>
-                    <li>available 20 € Vouchers: {details.amountof20}</li>
-                    <li>available 30 € Vouchers: {details.amountof30}</li>
-                </ul>
-            </div>
+                <div className="voucher">
+                    <ul>
+                        <li>voucher: {details.company_name} </li>
+                        <li>description: {details.description} </li>
+                        <li>available 10 € Vouchers: {details.amountof10}</li>
+                        <li>available 20 € Vouchers: {details.amountof20}</li>
+                        <li>available 30 € Vouchers: {details.amountof30}</li>
+                    </ul>
+                </div>
             <div className="checkboxes">
                 <input type="checkbox"/>
                 <label>10 €</label>
