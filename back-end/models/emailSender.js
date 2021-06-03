8164135f491698ebab2b1a9568f600085f28c6d0
bekhzod-ab <bekhzod.ab@gmail.com>
2021-06-03 16:08:24 +0200

    voucher buy/ confirmation email

---
+++ back-end/models/emailSender.js
@@ -0,0 +1,31 @@
+const nodemailer = require("nodemailer");
+const transporter = nodemailer.createTransport({
+  host: "mail.coding-school.org",
+  port: 465,
+  auth: {
+    user: "coupykauf@coding-school.org",
+    pass: "G5Sc&b*5@_y[",
+  },
+  tls: {
+    // do not fail on invalid certs
+    rejectUnauthorized: false,
+  },
+});
+function sendEmail( email, subject,  message, callback) {
+  const mailOption = {
+    from: "coupykauf@coding-school.org",
+    to: email,
+    subject: subject,
+    html:  message,
+  };
+  transporter.sendMail(mailOption, function (error, info) {
+    if (error) {
+      console.log(error);
+      callback(false);
+    } else {
+      console.log(info.response);
+      callback(true);
+    }
+  });
+}
+module.exports = { sendEmail };
