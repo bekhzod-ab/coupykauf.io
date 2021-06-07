6caef415e0803d9c67bfda03f513ce7661d5106d
Sandra Almoli <almolisandra@gmail.com>
2021-06-07 10:12:27 +0200

    about us text

--- front-end/src/pages/AboutUsPage.js
+++ front-end/src/pages/AboutUsPage.js
@@ -15,29 +15,35 @@ const AboutUsPage = () =>{
                 <img src={Bek} alt='missing' />
                 <figcaption>Bekhzod Abdulaev</figcaption>
                 <figcaption>Back-end Developer</figcaption>
+                <figcaption>Tech Lead</figcaption>
+                <figcaption>QA</figcaption>
                 <figcaption><a href="https://github.com/bekhzod-ab"><FaGithub className="icon" /></a></figcaption>
             </figure>
             <figure>
                 <img src={Sandra} alt='missing' />
                 <figcaption>Sandra Almoli</figcaption>
                 <figcaption>Front-end Developer</figcaption>
+                <figcaption>Product Owner</figcaption>
+                <figcaption>UI/UX Design</figcaption>
                 <figcaption><a href="https://github.com/Almolis"><FaGithub className="icon" /></a></figcaption>
             </figure>
             <figure>
                 <img src={Karl} alt='missing' />
                 <figcaption>Karl Langer</figcaption>
                 <figcaption>Front-end Developer</figcaption>
+                <figcaption>UI/UX Design</figcaption>
+                <figcaption>Responsive Design</figcaption>
                 <figcaption><a href="https://github.com/karllanger"><FaGithub className="icon" /></a></figcaption>
             </figure>
             </div>
 
             <div> 
                 <h3>BACKGROUND</h3>
-                <p>We are three graduates from the DCI .... </p>
+                <p>We are three graduates from the Digital Career Institute, where we got training in Full-Stack Web Development. All three of us started this course without any previous experience in Web Development. By the end of the course we had acquired various technical skills including HTML, CSS, JavaScript and React, as well as NodeJS and MongoDB.</p>
                 <h3>PROJECT IDEA</h3>
-                <p>The COVID crisis inspired us to build a tool for local businesses that would help them survive in times like these</p>
+                <p>The COVID crisis inspired us to build a tool for local businesses that would help them survive in times like these. We wanted to make an app where these small businesses can offer vouchers to customers which can be redeemed at a later date. This way, people's favorite local businesses can earn some revenue during closures and customers can help make that happen. Another use of this app would be selling gift-cards during normal times, previously only possible with tech giants such as Amazon. Regular customers can further support these businesses by buying gift-cards for their friends and family from their favorite hairdresser, cafe, independent cinema etc.</p>
                 <h3>THE PROCESS</h3>
-                <p>The tools that we used. The technologies.... </p>
+                <p>Firstly, we wanted to develop an intuitive app where customers and businesses would have to take no more than 3-4 steps to achieve their goal. For this, we first started coming up with the UX design using Figma. Then, we defined the bigger, weekly goals, according to which we defined our roles and smaller, daily tasks using Trello. We checked and coordinated our work on a daily basis, and supported each other on difficult tasks regardless of our main role.</p>
             </div>
         </div>
     )
